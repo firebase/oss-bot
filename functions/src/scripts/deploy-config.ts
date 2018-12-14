7ee8179d5a0b4008370fb6f608c7e2c19eb252bd
Sam <samstern@google.com>
2018-12-14 14:25:50 -0800

    Massive speedup to config deploys

--- functions/src/scripts/deploy-config.ts
+++ functions/src/scripts/deploy-config.ts
@@ -1,6 +1,9 @@
 import * as fs from "fs";
+import { type } from "os";
 
 const firebase = require("firebase-tools");
+
+type StringMap = { [s: string]:string };
 
 // Source: https://gist.github.com/penguinboy/762197
 function flattenObject(ob: any): any {
@@ -27,6 +30,19 @@ function flattenObject(ob: any): any {
   return toReturn;
 }
 
+function toFlatMap(ob: any): StringMap {
+  const flattened = flattenObject(ob);
+  const result: StringMap = {};
+  for (const key in flattened) {
+    const cleanKey = key.toLowerCase();
+    const cleanVal = flattened[key];
+
+    result[cleanKey] = cleanVal;
+  }
+
+  return result;
+}
+
 async function deployConfig(configFile: string, project: string) {
   const configFileString = fs.readFileSync(configFile).toString();
   const config = {
@@ -35,24 +51,69 @@ async function deployConfig(configFile: string, project: string) {
     }
   };
 
-  const flatConfig = flattenObject(config);
+  const newConfig = toFlatMap(config);
 
   console.log(`Deploying ${configFile} to ${project}.`);
 
-  // Construct key=val args for config.set
-  const args = [];
-  for (const key in flatConfig) {
-    const cleanKey = key.toLowerCase();
-    const cleanVal = flatConfig[key];
-
-    args.push(`${cleanKey}=${cleanVal}`);
-  }
-  console.log(args);
-
-  // Unset the 'runtime' config variable and all children
-  await firebase.functions.config.unset(["runtime"], {
+  const current = await firebase.functions.config.get("runtime", {
     project: project
   });
+
+  const currentConfig = toFlatMap({
+    runtime: current
+  });
+
+  const keysRemoved = [];
+  const keysAddedOrChanged = [];
+
+  for (const key in newConfig) {
+    const newVal = "" + newConfig[key];
+    const currentVal = "" + currentConfig[key];
+
+    if (newVal === currentVal) {
+      continue;
+    }
+
+    if (newVal === "" && currentVal !== "") {
+      console.log(`REMOVED: ${key}`);
+      console.log(`\tcurrent=${currentVal}`);
+      keysRemoved.push(key);
+    } else {
+      console.log(`CHANGED: ${key}`);
+      console.log(`\tcurrent=${currentVal}`);
+      console.log(`\tnew=${newVal}`);
+      keysAddedOrChanged.push(key);
+    }
+  }
+
+  const args = [];
+  if (keysRemoved.length > 0) {
+    // If anything is removed we need to nuke and start over
+    for (const key in newConfig) {
+      const val = newConfig[key];
+      args.push(`${key}=${val}`);
+    }
+
+    // Unset the 'runtime' config variable and all children
+    await firebase.functions.config.unset(["runtime"], {
+      project: project
+    });
+  } else {
+    // Otherwise we can just update what changed
+    for (const key of keysAddedOrChanged) {
+      const val = newConfig[key];
+      args.push(`${key}=${val}`);
+    }
+  }
+
+  // If no changes, we're done
+  if (args.length == 0) {
+    console.log("No config changes.")
+    return;
+  } 
+    
+  // Log out everything that is changing
+  console.log(args);
 
   // Set the new config
   await firebase.functions.config.set(args, {
